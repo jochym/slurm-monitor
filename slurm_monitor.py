78822ee53fe36b6aa040c13dd28e700d9253bb7d
Pawel T. Jochym <pawel.jochym@ifj.edu.pl>
2021-06-22 17:46:51 +0200

    Pass api url to content

--- slurm_monitor.py
+++ slurm_monitor.py
@@ -11,7 +11,7 @@ resturl='http://10.0.1.20/slurm-restapi/'
 
 @app.route("/")
 def index():
-    return render_template('content.html', selected="")
+    return render_template('content.html', resturl=resturl, selected="")
 
 
 @app.route("/jobs")
